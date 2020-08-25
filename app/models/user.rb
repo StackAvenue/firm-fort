6a665cafc7f8dfa0da9128315ca0b9b426f564f8
Apeksha Singh <apekshasingh664@gmail.com>
2020-08-25 18:58:24 +0530

    Added association on user  model and validations for first_name, email and gender columns

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  belongs_to :organisation
+  belongs_to :role
+  validates :first_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :gender, inclusion: { in: %w(male female other) }
 end
