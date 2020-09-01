dc1ec00e388c7092b777b50acea470f2edce4788
Apeksha Singh <apekshasingh664@gmail.com>
2020-09-01 10:04:41 +0530

    added tests for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -21,7 +21,44 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "first_name must be present" do
+    user = build(:user, :first_name => nil)
+    user.valid?
+    assert_not_empty user.errors[:first_name]
+  end
+
+  test "email must be present" do
+    user = build(:user, :email => nil)
+    user.valid?
+    assert_not_empty user.errors[:email] 
+  end
+
+  test "designation must be present" do
+    user = build(:user, :designation => nil)
+    user.valid?
+    assert_empty user.errors[:designation]
+  end
+  
+  test "age must be present" do
+    user = build(:user, :age => nil)
+    user.valid?
+    assert_empty user.errors[:age]
+  end
+
+  test "organisation must be present" do
+    user = build :user, organisation: nil
+    user.valid?
+    assert_not_empty user.errors[:organisation]
+  end 
+  
+  test "role must be present" do
+    user = build :user, role: nil
+    user.valid?
+    assert_not_empty user.errors[:role]
+  end  
+
+  test "save user successfully" do 
+    user = create(:user)
+    assert true, user.save
+  end
 end
