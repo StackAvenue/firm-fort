c601e0025cbadac7cfb1e333c938de61476a1ecf
Apeksha Singh <apekshasingh664@gmail.com>
2020-09-02 17:57:02 +0530

    fixed class and function name and removed redundant code

--- db/seeds/user.seed_data.rb
+++ db/seeds/user.seed_data.rb
@@ -4,21 +4,21 @@ $LOAD_PATH << '.'
 require 'db/seeds/organisation.seed_data.rb'
 require 'db/seeds/role.seed_data.rb'
 
-class SeedUser
-  def self.create_user
+class User
+  def self.seed
     user = User.new(
-      :first_name        => "Apeksha",
-      :last_name         => "Singh",
-      :email             => "apekshasingh664@gmail.com",
-      :designation       => "SuperAdmin",
-      :age               => 21,
-      :gender            => "female",
-      :address           => "xyz",
-      :twitter_url       => "abc",
-      :linkedin_url      => "abc",
-      :github_url        => "abc",
-      :organisation_id   => Organisation.first.id,
-      :role_id           => Role.first.id
+      first_name:        "Apeksha",
+      last_name:         "Singh",
+      email:             "apekshasingh664@gmail.com",
+      designation:       "SuperAdmin",
+      age:                21,
+      gender:            "female",
+      address:           "xyz",
+      twitter_url:       "abc",
+      linkedin_url:      "abc",
+      github_url:        "abc",
+      organisation_id:   Organisation.first.id,
+      role_id:           Role.first.id
     )
     user.save!
   end
