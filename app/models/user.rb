7c118d1dedc9e040089c1be912088c763b4dd796
Apeksha Singh <apekshasingh664@gmail.com>
2020-09-03 17:31:17 +0530

    migrations to add devise to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,10 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   #associations
   belongs_to :organisation
   belongs_to :role
