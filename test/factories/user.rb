ac24bdf91e86d57bebadb78626e7e23446ac7b6d
Apeksha Singh <apekshasingh664@gmail.com>
2020-09-04 15:36:27 +0530

    updated users factory data

--- test/factories/user.rb
+++ test/factories/user.rb
@@ -3,12 +3,12 @@ FactoryBot.define do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
-    designation { Faker::Lorem.word }
     gender { random = ['male', 'female', 'other'].sample }
     address { Faker::Address.city }
     twitter_url {Faker::Internet.name}
     linkedin_url {Faker::Internet.name}
     github_url {Faker::Internet.name}
+    password { Faker::Number.number(digits: 8) }
     association :organisation
     association :role
   end 
