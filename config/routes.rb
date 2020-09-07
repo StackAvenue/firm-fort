99c0c66a772467f2ce946ce6c078cb691f51cf91
Apeksha Singh <apekshasingh664@gmail.com>
2020-09-07 15:27:51 +0530

    added routes for dashboard page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'page/dashboard'
+  get 'pages/dashboard'
   devise_for :users, controllers: {
         sessions: 'users/sessions'
       }
   
+  root  'page#dashboard'
 end
