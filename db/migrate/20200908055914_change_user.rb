d7f60eba3992be988601d64c50905fba31f94316
Apeksha Singh <apekshasingh664@gmail.com>
2020-09-08 11:36:16 +0530

    migrations to change users gender and designation to null

---
+++ db/migrate/20200908055914_change_user.rb
@@ -0,0 +1,9 @@
+class ChangeUser < ActiveRecord::Migration[6.0]
+  def up
+    change_table :users do |t|
+      change_column_null :users, :gender, null: true
+      change_column_null :users, :designation, null: true
+      change_column_default :users, :designation, from: true , to: "Member"
+    end
+  end
+end
