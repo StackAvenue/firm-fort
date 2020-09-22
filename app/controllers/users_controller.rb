4822bf7f4cd514d0837db07b3e9e90e2f4ac64fa
Apeksha Singh <apekshasingh664@gmail.com>
2020-09-22 16:24:24 +0530

    added one line space

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
   end
 
   private
+  
   def user_params
     params.require(:user).permit(:first_name, :last_name, :gender, :role_id, :organisation_id, :email, :password, :password_confirmation)
   end
