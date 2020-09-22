5c08175020b0252cd0a080439da1f54a74c0545c
Apeksha Singh <apekshasingh664@gmail.com>
2020-09-22 16:25:51 +0530

    added modules for full name and initials of firstname and last name

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,10 @@
 module HomeHelper
+
+  def full_name(user)
+    "#{current_user.first_name + " " + current_user.last_name}"
+  end
+
+  def first_and_last_name_initials(user)
+    "#{current_user.first_name.slice(0,1)+current_user.last_name.slice(0,1)}"
+  end
 end
