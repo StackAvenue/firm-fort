7d0724cab5da221681b31e4892a0af68dc9632d0
Apeksha Singh <apekshasingh664@gmail.com>
2020-09-29 14:55:07 +0530

    updated users controllers methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,23 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @users = User.find(params[:id])
+    @user = User.find(params[:id])
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+  
+    if @user.update(user_params)
+      redirect_to users_path
+    end
   end
   
   def destroy
-    @users = User.find(params[:id])
-    @users.destroy
+    @user = User.find(params[:id])
+    @user.destroy
    
     redirect_to users_path
   end
@@ -19,6 +30,6 @@ class UsersController < ApplicationController
   private
   
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :gender, :role_id, :organisation_id, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :gender, :age, :role_id, :organisation_id, :email, :password, :password_confirmation)
   end
 end
