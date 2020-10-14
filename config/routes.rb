5a6e66a1bc0d89262a82b471f466b37d835fbca1
Apeksha Singh <apekshasingh664@gmail.com>
2020-10-14 21:04:22 +0530

    added routes for devise invitations

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'users/index'
   devise_for :users, controllers: {
         sessions: 'users/sessions',
-        registrations: "users/registrations"
+        registrations: "users/registrations",
+        invitations: "users/invitations"
       }
   devise_scope :user do
     get '/users/sign_out' => 'users/sessions#destroy'
