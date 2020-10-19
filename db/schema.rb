edcd2afddbf2f60f23609a4357afa016c5e3047a
Apeksha Singh <apekshasingh664@gmail.com>
2020-10-19 22:33:26 +0530

    added column invitation_status to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_06_121709) do
+ActiveRecord::Schema.define(version: 2020_10_15_091045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2020_10_06_121709) do
     t.string "invited_by_type"
     t.bigint "invited_by_id"
     t.integer "invitations_count", default: 0
+    t.string "invitation_status"
     t.index ["email", "organisation_id"], name: "index_users_on_email_and_organisation_id", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["invitations_count"], name: "index_users_on_invitations_count"
