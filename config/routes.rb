7f0aabb952ed637b3f2e7e7acab5c79659dcc270
Apeksha Singh <apekshasingh664@gmail.com>
2020-10-22 15:53:43 +0530

    added routes for resend invitation

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'users/sessions#destroy'
     get '/users/invitation/pending' => 'users/invitations#pending'
+    get '/user/invitation/resend/:id' => 'users/invitations#resend', as: 'user_invitation_resend'
   end
 
   resources :users
