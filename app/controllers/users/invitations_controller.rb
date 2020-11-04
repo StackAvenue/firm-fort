63e3bf96c3ce7eea479b3416ac7655da2062d3e7
Apeksha Singh <apekshasingh664@gmail.com>
2020-11-04 21:03:53 +0530

    fixed create action

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,14 +1,17 @@
 class Users::InvitationsController < Devise::InvitationsController
+  before_action :authenticate_inviter!, only: [:new, :create, :pending]
   
   def create
     @user = User.new(user_params)
     @user.organisation_id = current_user.organisation_id
     @user.invitation_status = "Pending"
-    if @user.invite!
-      redirect_to users_path
-    else 
-      render 'new'
-    end
+    @user.password = "password"
+      if @user.valid? && @user.invite!
+        @notice = "Invitation Sent"
+        render 'new'
+      else 
+        render 'new'
+      end
   end
 
   def pending
@@ -23,6 +26,6 @@ class Users::InvitationsController < Devise::InvitationsController
 
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :role_id, :organisation_id, :email, :id)
+    params.require(:user).permit(:first_name, :last_name, :role_id, :organisation_id, :email, :id, :invitation_token, :invitation_status)
   end 
 end
