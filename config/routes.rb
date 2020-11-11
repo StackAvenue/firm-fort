1c0c62390209a80572b8f6b0d2b0965d5d97543a
Apeksha Singh <apekshasingh664@gmail.com>
2020-11-11 16:45:53 +0530

    added routes for users search action

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'users/sessions#destroy'
     get '/users/invitation/pending' => 'users/invitations#pending'
     get '/user/invitation/resend/:id' => 'users/invitations#resend', as: 'user_invitation_resend'
+    get '/search' => 'users#search'
   end
 
   resources :users
