287255f7dbf0a9476dfa4a870ca32159a8d16562
Apeksha Singh <apekshasingh664@gmail.com>
2020-11-17 15:27:30 +0530

    applied proper indentation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,7 @@ class ApplicationController < ActionController::Base
   include Pagy::Backend
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-
-
   protected
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit :accept_invitation, keys: [:first_name, :last_name, :invitation_status]
   end
