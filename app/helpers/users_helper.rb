05b5ce45a7c4dbed59f02e52908fcb51c3562836
Apeksha Singh <apekshasingh664@gmail.com>
2020-11-17 15:49:01 +0530

    added more helper methods for user

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,17 @@
 module UsersHelper
 
+  def current_user_full_name(user)
+    "#{current_user.first_name + " " + current_user.last_name}"
+  end
+
+  def first_and_last_name_initials(user)
+    "#{current_user.first_name.slice(0,1)+current_user.last_name.slice(0,1)}"
+  end
+
+  def full_name(user)
+    "#{user.first_name + " " + user.last_name}"
+  end
+  
   def all_users_in_organisation(user)
     user.organisation_id == current_user.organisation_id && 
                 (user.invitation_status == "Accepted" || user.invitation_status == nil)
