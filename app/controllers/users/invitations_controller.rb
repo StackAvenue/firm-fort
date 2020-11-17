3a29fc863958978dbca526a97418a07eea0cd9a7
Apeksha Singh <apekshasingh664@gmail.com>
2020-11-17 15:46:38 +0530

    refactored create, pending and resend actions

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -3,29 +3,23 @@ class Users::InvitationsController < Devise::InvitationsController
   
   def create
     @user = User.new(user_params)
-    @user.organisation_id = current_user.organisation_id
-    @user.invitation_status = "Pending"
-    @user.password = "password"
-      if @user.valid? && @user.invite!
-        @notice = "Invitation Sent"
-        render 'new'
-      else 
-        render 'new'
-      end
+    @notice = "Invitation Sent" if @user.valid? && @user.invite!
+    render 'new'
   end
 
   def pending
-    @pagy, @user = pagy(User.where(invitation_status: "Pending"))
+    @pagy, @pending_users = pagy(User.where(invitation_status: "Pending"))
   end
 
   def resend
     @user = User.find(params[:id])
     @user.invite!
-    redirect_to users_invitation_pending_path
+    redirect_to users_invitation_pending_path, notice: "Invitation Sent" 
   end
 
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :role_id, :organisation_id, :email, :id, :invitation_token, :invitation_status)
+    params.require(:user).permit(:first_name, :last_name, :role_id, :organisation_id, :email, 
+                  :id, :invitation_token, :invitation_status, :password)
   end 
 end
