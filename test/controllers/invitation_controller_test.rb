f0ab68398d6e6f327d2a996f3db562a4d8d96551
Apeksha Singh <apekshasingh664@gmail.com>
2020-11-17 17:02:04 +0530

    removed redundant code

--- test/controllers/invitation_controller_test.rb
+++ test/controllers/invitation_controller_test.rb
@@ -9,32 +9,28 @@ class Users::InvitationsControllerTest < ActionController::TestCase
   end
   
   test 'user should be created when inviting a new user' do
-    user = @new_user
     assert_difference 'User.count', 1 do
-      user.invite!(@user)
+      @new_user.invite!(@user)
     end
   end
 
   test 'email should be sent when inviting a new user' do
-    user = @new_user
     assert_emails 1 do
-      user.invite!(@user)
+      @new_user.invite!(@user)
     end
   end
 
   test 'user should update his password to accept invitation' do
-    user = @new_user
-    user.invite!(@user)
-    user.password = Faker::Number.number(digits: 8)
-    user.password_confirmation = user.password
-    assert true, user.accept_invitation!
+    @new_user.invite!(@user)
+    @new_user.password = Faker::Number.number(digits: 8)
+    @new_user.password_confirmation =  @new_user.password
+    assert true,  @new_user.accept_invitation!
   end
 
   test 'resend invitation email to user' do
-    user = @new_user
     assert_emails 2 do
-      user.invite!(@user)
-      user.invite!(@user)
+      @new_user.invite!(@user)
+      @new_user.invite!(@user)
     end
   end
 end
