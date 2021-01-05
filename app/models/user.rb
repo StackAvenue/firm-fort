26dae467c78a755c973c3e77876d577d440f88bf
Apeksha Singh <apekshasingh664@gmail.com>
2021-01-05 17:06:29 +0530

    added omniauth  to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,8 +22,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
   #associations
   belongs_to :organisation
   belongs_to :role
@@ -34,5 +33,17 @@ class User < ApplicationRecord
   validates :role, presence: true
   validates :organisation, presence: true
   validates :email, presence: true
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+
+    unless user
+      user = User.create(first_name: data['first_name'],
+        email: data['email'],
+        password: Devise.friendly_token[0,20])
+    end
+    user
+  end
 end
 
